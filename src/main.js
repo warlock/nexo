d3fcee3c2a4cbd531e6f621edacd7d59a8769a6f
Josep Subils <josep@spellbook.io>
2017-08-16 13:29:02 +0200

    [wip] render

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ const n = {
     list: {
       html: (state, attr) => `
       <p>${state.name} is ${attr.nom}</p>
-      <user></user>      
+      <ex></ex>            
+      <user></user>
       `,
       attr: {
         nom: 'josep'
@@ -15,16 +16,17 @@ const n = {
       onload: (state, set) => {
       },
       ready: (state, set) => {
-        /*
-        console.log('Tornant a pintar !')
+        console.log('Component list ready!')
         setInterval(() => {
-          setState('name', tools.random())
+          set('name', tools.random())
         }, 1000)
-        */
       }
     },
     user: {
-      html: (state, attr) => `<p>El nom del usuari es: ${attr.nom}</p>`,
+      html: (state, attr) => `<p>El nom del usuari es: ${attr.nom}</p>
+      <ex></ex>
+      <ex></ex>
+      `,
       attr: {
         nom: 'user'
       }
@@ -54,17 +56,21 @@ const n = {
       return element.map(n.render)
     } else {
       if (element.type === 'Element' && undefined !== n.components[element.tagName]) {
-        console.log(element.tagName)
         const htmlSchema = n.components[element.tagName].html(n.state, n.makeAttr(element.tagName, element.attributes))
         const jsonSchema = tojson.parse(htmlSchema)
         if (undefined !== jsonSchema.children && jsonSchema.children.length > 0) jsonSchema.children = n.render(jsonSchema.children)
-        return jsonSchema // Can return an array
+        const loop = n.render(jsonSchema)
+        return {
+          type:"Element",
+          tagName:"div",
+          attributes: element.attributes,
+          children: loop
+        }
       } else return element
     }
   },
   done () {
     const newSchema = n.render(n.schema)
-    console.log(JSON.stringify(newSchema))
     const htmlSchema = tohtml(newSchema)
     n.main.innerHTML = htmlSchema
   }
